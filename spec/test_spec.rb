ebae7650825b806a441685d5ed46683fadf2cdd6
shinn <matsuzaki@startup-technology.com>
2021-12-08 14:49:24 +0900

    [Modify] mysqlの導入

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Itemモデルのテスト', type: :model do
   describe 'バリデーションのテスト' do
     it '空でないこと' do
-      expect(1 + 1).to eq 2
+      expect(1 + 1).to eq 3
     end
   end
 end
